4ac3615830049fa732d8e9af5d81a363cce797c2
Hagee <hagee.ktr@outlook.com>
2025-06-27 14:13:25 +0300

    [Feature]: Add challenge form react component

---
+++ client/src/components/ChallengeForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function ChallengeForm({ onChallengeCreated }) {
+    const [formData, setFormData] = useState({ title: '', description: '', start_date: '', end_date: '' });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            fetch('/challenges', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            })
+                .then((r) => r.json())
+                .then(onChallengeCreated)
+        };
+    }
+
+        return (
+            <form onSubmit={handleSubmit} className="form">
+                <input name="title" value={formData.title} onChange={handleChange} placeholder="Challenge Title" />
+                <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Description"/>
+                <input name="start_date" value={formData.start_date} onChange={handleChange} />
+                <input name="end_date" value={formData.end_date} onChange={handleChange} />
+                <button type="submit">Add Challenge</button>
+            </form>
+        );
+}
+
+export default ChallengeForm;
