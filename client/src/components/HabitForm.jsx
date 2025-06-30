ae7bbdd2d8db9a7738766f290d518515cf5e22b7
Hagee <hagee.ktr@outlook.com>
2025-06-30 16:46:38 +0300

    fix toggleCompletion

--- client/src/components/HabitForm.jsx
+++ client/src/components/HabitForm.jsx
@@ -128,16 +128,16 @@ function HabitForm() {
   const toggleCompletion = (habit) => {
     const updatedCompleted = !habit.completed;
 
-    fetch(`${process.env.REACT_APP_API_URL}/habits/${habit.id}`, {
-  method: "PATCH",
-  headers: { "Content-Type": "application/json" },
-  credentials: "include",
-  body: JSON.stringify({
-    name: values.name,
-    description: values.description,
-    frequency: values.frequency,
-    completed: !!values.completed  
-  })
+     fetch(`${process.env.REACT_APP_API_URL}/habits/${habit.id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({
+      name: habit.name,
+      description: habit.description,
+      frequency: habit.frequency,
+      completed: updatedCompleted
+    })
 })
       .then((res) => {
         if (!res.ok) throw new Error("Failed to update habit status");
