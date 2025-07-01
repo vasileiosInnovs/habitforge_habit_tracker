9a87f706f6c4b0ca5e2d3830b3b391a33a27389d
Hagee <hagee.ktr@outlook.com>
2025-07-01 16:33:32 +0300

    [Fix]: fix import issues and style challenge

--- client/src/components/MyDay.jsx
+++ client/src/components/MyDay.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import '../index.css';
-
+import { NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function MyDay() {
