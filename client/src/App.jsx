65b297632e7c30876618eaa53ae3d5ed61bbac7f
Hagee <hagee.ktr@outlook.com>
2025-07-02 08:02:44 +0300

    [Fix]: correct path to scrolltotop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import ScrollToTop from "./ScrollToTop";
 
+import ScrollToTop from "./components/ScrollToTop";
 import ProtectedRoute from './components/ProtectedRoute';
 import NavBar from './components/NavBar';
 import ProgressLog from "./components/ProgressLog";
